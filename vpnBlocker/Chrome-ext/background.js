37966c1bbadd4c6dfa67b22d22f7b96248cbaba6
Anant <anant.kandikuppa@gmail.com>
2016-04-06 02:36:30 +0530

    Migrated server.py from BaseHTTPServer to cherrypy

--- vpnBlocker/Chrome-ext/background.js
+++ vpnBlocker/Chrome-ext/background.js
@@ -1,25 +1,46 @@
+function extractDomain(url){
+    var domain;
+    if(url.indexOf("://") > -1){
+        domain = url.split('/')[2];
+    }
+    else{
+        domain = url.split('/')[0];
+    }
+    domain = domain.split(':')[0];
+
+    return domain;
+}
+
+var SERVER_URL = "http://localhost:8088/";
+
 chrome.webRequest.onBeforeRequest.addListener(
         function(details){
             var bkg = chrome.extension.getBackgroundPage();
             bkg.console.log('foo');
             url = details.url;
-            var domain = url.match(/^[\w-]+:\/*\[?([\w\.:-]+)\]?(?::\d+)?/)[1];
+            timestamp = details.timeStamp;
+            requestId = details.requestId;
+            var domain = extractDomain(url);
             bkg.console.log(domain);
-            if(domain != 'localhost:8088'){
-                SERVER_URL = "http://localhost:8088/";
+            if(domain != 'localhost'){
+                //SERVER_URL = "http://localhost:8088/";
                 var xhr = new XMLHttpRequest();
-                xhr.open("POST",SERVER_URL,true);
-                xhr.send(url);
+                var data = new FormData();
+                data.append('domain',domain);
+                data.append('timestamp',timeStamp);
+                data.append('requestId',requestId);
+                xhr.open("POST",data,true);
+                xhr.send(data);
             }
         },
         {urls:["<all_urls>"],
             types:["main_frame"]}
         );
 
-function toBlock(url){
-    SERVER_URL = "http://localhost:8088/";
+function toBlock(domain,timeStamp){
+    //SERVER_URL = "http://localhost:8088/";
     var xhr = new XMLHttpRequest();
-    xhr.open("GET",SERVER_URL + url,false);
+    xhr.open("GET",SERVER_URL + '?domain='+domain+'&time='+ timeStamp,false);
     xhr.send();
     return xhr.responseText;
 }
@@ -27,11 +48,12 @@ function toBlock(url){
 chrome.webRequest.onHeadersReceived.addListener(
         function(details){
             url = details.url;
-            var domain = url.match(/^[\w-]+:\/*\[?([\w\.:-]+)\]?(?::\d+)?/)[1];
-            if(domain != 'localhost:8088'){
-                if(toBlock(url)){
+            timeStamp = details.timeStamp;
+            var domain = extractDomain(url);
+            if(domain != 'localhost'){
+                if(toBlock(domain,timeStamp)){
                     return {
-                    redirectUrl : "http://localhost:8088/blocked.html"
+                    redirectUrl : "http://localhost:8088/blocked"
                     };
                 }
             }    
--- vpnBlocker/server.py
+++ vpnBlocker/server.py
@@ -1,28 +1,48 @@
-import time
-import BaseHTTPServer
+import cherrypy
+from expiringdict import ExpiringDict
+import dns.resolver
 
-HOST_NAME = 'localhost'
-PORT_NUMBER = 8088
+url_cache = ExpiringDict(max_len = 1000,max_age_seconds=600)
 
-class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
-    def do_GET(s):
-        s.send_response(200)
-        s.send_header("Content-type", "text/html")
-        s.end_headers()
-        print s.path
-        if s.path == '/blocked.html':
-            with open('blocked.html','rb') as f:
-                s.wfile.write(f.read())
-        else:
-            s.wfile.write("1")
-        
+BLOCK_HTML = """ 
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Page Blocked</title>
+    </head>
+
+    <body>
+        Page blocked due to suspected VPN usage
+    </body>
+</html>
+
+"""
+def checkIfSeen(ip_tuple,rec_time):
+    return True
+
+class VPNBlocker(object):
+    @cherrypy.expose
+    def query(self,request_id,rec_time):
+        print request_id,rec_time
+        ip_tuple = url_cache.get(request_id)
+        seen = checkIfSeen(ip_tuple,rec_time)
+        if(seen):
+            return '1'
+        return '0'
+    
+    @cherrypy.expose
+    def record(self,domain,request_id,send_time):
+        print domain,send_time
+        answers = dns.resolver.query(domain,'A')
+        for rdata in answers:
+            url_cache[request_id] = (rdata,send_time)
+
+        return ''
+
+    @cherrypy.expose
+    def block(self):
+        return BLOCK_HTML
+
 if __name__ == '__main__':
-    server_class = BaseHTTPServer.HTTPServer
-    httpd = server_class((HOST_NAME, PORT_NUMBER), MyHandler)
-    print time.asctime(), "Server Starts - %s:%s" % (HOST_NAME, PORT_NUMBER)
-    try:
-        httpd.serve_forever()
-    except KeyboardInterrupt:
-        pass
-    httpd.server_close()
-    print time.asctime(), "Server Stops - %s:%s" % (HOST_NAME, PORT_NUMBER)
+    cherrypy.quickstart(VPNBlocker())
+
