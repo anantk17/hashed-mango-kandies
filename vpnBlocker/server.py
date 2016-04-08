1f0ad0e42323a207911ff2405cfcb173c1cec1ab
Anant <anant.kandikuppa@gmail.com>
2016-04-08 21:58:40 +0530

    Sniffed DNS packets to obtain domain-address mappings

--- vpnBlocker/server.py
+++ vpnBlocker/server.py
@@ -3,6 +3,7 @@ from expiringdict import ExpiringDict
 from ip_store import *
 from struct import *
 import multiprocessing
+from multiprocessing import Manager
 from scapy.all import *
 
 url_cache = ExpiringDict(max_len = 1000,max_age_seconds=600)
@@ -28,78 +29,68 @@ manager = IpStoreManager()
 manager.start()
 ipStore = manager.IpStore()
 
-def sniffer(ipStore):
-    """import socket,sys,time
-    
-    try:
-        s = socket.socket( socket.AF_PACKET , socket.SOCK_RAW , socket.ntohs(0x0003))
-    except socket.error , msg:
-        print 'Socket could not be created. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
-        sys.exit()
- 
-# receive a packet
-    while True:
-        packet = s.recvfrom(65565)
-     
-    #packet string from tuple
-        packet = packet[0]
-     
-    #parse ethernet header
-        eth_length = 14
-     
-        eth_header = packet[:eth_length]
-        eth = unpack('!6s6sH' , eth_header)
-        eth_protocol = socket.ntohs(eth[2])
-    #print 'Destination MAC : ' + eth_addr(packet[0:6]) + ' Source MAC : ' + eth_addr(packet[6:12]) + ' Protocol : ' + str(eth_protocol)
- 
-    #Parse IP packets, IP Protocol number = 8
-        if eth_protocol == 8 :
-        #Parse IP header
-        #take first 20 characters for the ip header
-            ip_header = packet[eth_length:20+eth_length]
-         
-        #now unpack them :)
-            iph = unpack('!BBHHHBBH4s4s' , ip_header)
- 
-            version_ihl = iph[0]
-            version = version_ihl >> 4
-            ihl = version_ihl & 0xF
- 
-            iph_length = ihl * 4
- 
-            ttl = iph[5]
-            protocol = iph[6]
-            s_addr = socket.inet_ntoa(iph[8]);
-            d_addr = socket.inet_ntoa(iph[9]);
- 
-        #print 'Version : ' + str(version) + ' IP Header Length : ' + str(ihl) + ' TTL : ' + str(ttl) + ' Protocol : ' + str(protocol) + ' Source Address : ' + str(s_addr) + ' Destination Address : ' + str(d_addr)
-            time_stamp = time.time()*1000.00
-            ipStore.insert(str(s_addr),time_stamp)
-            print str(s_addr), time_stamp"""
-    sniff(iface='ppp0',prn=lambda x:ipStore.insert(str(x[IP].src),time.time()*1000.0))
+cacheManager = Manager()
+#cacheManager.start()
+dnsCache = cacheManager.dict()
+
+def sniffer(ipStore,dnsCache):
+    def on_sniff(pkt):
+        if pkt.haslayer(DNS):
+            domain_names = []
+            addresses = []
+            if pkt.ancount > 0 and isinstance(pkt.an,DNSRR):
+                domain_names.append(pkt.qd.qname)
+                #print pkt.ancount, pkt.qd.qname
+                for i in range(pkt.ancount):
+                    dnsrr = pkt.an[i]
+                    if dnsrr.type == 1:
+                        addresses.append(dnsrr.rdata)
+                    else:
+                        domain_names.append(dnsrr.rdata)
+                        #print dnsrr.rrname, dnsrr.rdata, dnsrr.type
+        
+                for domain in domain_names:
+                    dnsCache[domain.strip('.')] =  addresses
+                print domain_names, addresses
+
+
+        elif pkt.haslayer('IP'):
+            ip = pkt['IP']
+            ipStore.insert(str(ip.src),pkt.time*1000.0)
+
+    sniff(iface='ra0',prn=on_sniff,store=0)
         
 
-def checkIfSeen(ip_tuple,rec_time):
-    ip,start_time = ip_tuple
-    latest_seen = ipStore.query(ip)
-    print "IP",ip,"Timings",start_time,latest_seen,rec_time
-    print start_time - latest_seen 
-    print latest_seen - rec_time
-    if (start_time-2 <= latest_seen and latest_seen <= rec_time+2):
-        return True
-    else:
-        return False
+def checkIfSeen(domain_tuple,rec_time):
+    domain,start_time = domain_tuple
+    addresses = dnsCache.get(domain)
+    if addresses:
+        status = []
+        for ip in addresses:
+            latest_seen = ipStore.query(ip)
+            print "IP",ip,"Timings",start_time,latest_seen,rec_time
+            print start_time - latest_seen 
+            print latest_seen - rec_time
+            status.append(start_time-2 <= latest_seen and latest_seen <= rec_time+2)
+            #    return True
+            #else:
+            #    return False
+            #seen = [(start_time-2 <= latest_seen and latest_seen <= rec_time+2) for 
+        return status
+
+    return [False]
 
 class VPNBlocker(object):
     @cherrypy.expose
     def query(self,request_id,rec_time):
         print request_id,rec_time
-        ip_tuples = url_cache.get(request_id)
+        domain_tuple = url_cache.get(request_id)
         rec_time = time.time()*1000.0
-        if ip_tuples:
-            results = [checkIfSeen(x,rec_time) for x in ip_tuples]
+        if domain_tuple:
+            results = checkIfSeen(domain_tuple,rec_time)
             print results
             seen = any(results)
+            #seen = results
             print 'ProxyUsed',not(seen)
             if(seen):
                 return '0'
@@ -110,11 +101,11 @@ class VPNBlocker(object):
     @cherrypy.expose
     def record(self,domain,request_id,send_time):
         print "New POST"
-        answers = lan_dns_resolver.query(domain,'A')
-        url_cache[request_id] = []
-        for rdata in answers:
-            url_cache[request_id].append((str(rdata),int(send_time)))
-            print domain,rdata
+        #answers = lan_dns_resolver.query(domain,'A')
+        #url_cache[request_id] = []
+        #for rdata in answers:
+        url_cache[request_id] = (str(domain),int(send_time))
+            #print domain,rdata
         return ''
 
     @cherrypy.expose
@@ -123,7 +114,7 @@ class VPNBlocker(object):
 
 if __name__ == '__main__':
     #ip_tuple = ('192.168.0.1',time.time())
-    p = multiprocessing.Process(target = sniffer, args=(ipStore,))
+    p = multiprocessing.Process(target = sniffer, args=(ipStore,dnsCache))
     p.daemon = True
     p.start()
     #time.sleep(1)
--- vpnBlocker/sniffer.py
+++ vpnBlocker/sniffer.py
@@ -6,59 +6,31 @@ import socket, sys
 from struct import *
 import time
 from scapy.all import *
- 
-#Convert a string of 6 characters of ethernet address into a dash separated hex string
-#def eth_addr (a) :
-#  b = "%.2x:%.2x:%.2x:%.2x:%.2x:%.2x" % (ord(a[0]) , ord(a[1]) , ord(a[2]), ord(a[3]), ord(a[4]) , ord(a[5]))
-#  return b
- 
-#create a AF_PACKET type raw socket (thats basically packet level)
-#define ETH_P_ALL    0x0003          /* Every packet (be careful!!!) */
-"""
-try:
-#    s = socket.socket( socket.AF_PACKET , socket.SOCK_RAW , socket.ntohs(0x0003))
-except socket.error , msg:
-    print 'Socket could not be created. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
-    sys.exit()
- 
-# receive a packet
-while True:
-    packet = s.recvfrom(65565)
-     
-    #packet string from tuple
-    packet = packet[0]
-     
-    #parse ethernet header
-    eth_length = 14
-     
-    eth_header = packet[:eth_length]
-    eth = unpack('!6s6sH' , eth_header)
-    eth_protocol = socket.ntohs(eth[2])
-    #print 'Destination MAC : ' + eth_addr(packet[0:6]) + ' Source MAC : ' + eth_addr(packet[6:12]) + ' Protocol : ' + str(eth_protocol)
- 
-    #Parse IP packets, IP Protocol number = 8
-    if eth_protocol == 8 :
-        #Parse IP header
-        #take first 20 characters for the ip header
-        ip_header = packet[eth_length:20+eth_length]
-         
-        #now unpack them :)
-        iph = unpack('!BBHHHBBH4s4s' , ip_header)
- 
-        version_ihl = iph[0]
-        version = version_ihl >> 4
-        ihl = version_ihl & 0xF
- 
-        iph_length = ihl * 4
- 
-        ttl = iph[5]
-        protocol = iph[6]
-        s_addr = socket.inet_ntoa(iph[8]);
-        d_addr = socket.inet_ntoa(iph[9]);
- 
-        #print 'Version : ' + str(version) + ' IP Header Length : ' + str(ihl) + ' TTL : ' + str(ttl) + ' Protocol : ' + str(protocol) + ' Source Address : ' + str(s_addr) + ' Destination Address : ' + str(d_addr)
-        print 'Source Address' + str(s_addr)
-"""
+from scapy.layers.dns import DNSRR,DNS, DNSQR
+
+def sniffer(pkt):
+    if pkt.haslayer(DNS):
+        domain_names = []
+        addresses = []
+        if pkt.ancount > 0 and isinstance(pkt.an,DNSRR):
+            domain_names.append(pkt.qd.qname)
+            #print pkt.ancount, pkt.qd.qname
+            for i in range(pkt.ancount):
+                dnsrr = pkt.an[i]
+                if dnsrr.type == 1:
+                    addresses.append(dnsrr.rdata)
+                else:
+                    domain_names.append(dnsrr.rdata)
+                #print dnsrr.rrname, dnsrr.rdata, dnsrr.type
+        
+            print domain_names , addresses
+
+    elif pkt.haslayer('IP'):
+        ip = pkt['IP']
+        #print ip.src
+    print pkt.time*1000.0
+
+
 conf.sniff_promisc=False
-sniff(iface='ppp0',prn=lambda x: x.summary(),store=0)
+sniff(iface='ra0',prn=sniffer,store=0)
 
